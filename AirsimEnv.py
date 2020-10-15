1e18732b308575773eb011276b113cb803172823
seeeheng <siheng.teng@gmail.com>
2020-10-15 15:43:17 +0800

    environment ready

--- AirsimEnv.py
+++ AirsimEnv.py
@@ -2,6 +2,47 @@ import airsim
 import time
 import numpy as np
 
+class AirsimEnv():
+	def __init__(self, client):
+		self.client = client
+
+	def compute_reward(self):
+		collision_info = self.client.get_collision_info()
+		reward = -1
+		if collision_info.has_collided:
+			print("Collided with {}".format(collision_info.object_name))
+			if collision_info.object_name == "RLTarget_0":
+				reward += 100
+			elif collision_info.object_name != "RLTarget_0":
+				reward -= -20
+		print("Reward = {}".format(reward))
+		return reward
+
+	def is_done(self):
+		collision_info = self.client.get_collision_info()
+		if collision_info.has_collided:
+			return True
+		else:
+			return False
+
+# Sample of collision info
+"""
+<CollisionInfo> {   'has_collided': True,
+    'impact_point': <Vector3r> {   'x_val': 23.136844635009766,
+    'y_val': -0.7587664723396301,
+    'z_val': -0.4420730471611023},
+    'normal': <Vector3r> {   'x_val': -1.0,
+    'y_val': 0.0,
+    'z_val': -0.0},
+    'object_id': 241,
+    'object_name': 'TemplateCube_Rounded_84',
+    'penetration_depth': 0.03684478625655174,
+    'position': <Vector3r> {   'x_val': 21.154447555541992,
+    'y_val': 0.006373672280460596,
+    'z_val': 0.2329985797405243},
+    'time_stamp': 1602735741096291000}
+"""
+
 class AirsimClient():
     def __init__(self):
         self.client = airsim.CarClient()
--- main.py
+++ main.py
@@ -2,10 +2,28 @@ import numpy as np
 import time
 import airsim
 from AirsimClient import AirsimClient
+from AirsimEnv import AirsimEnv
+
+client = AirsimClient()
+env = AirsimEnv(client)
 
 while True:
-    client = AirsimClient()
-    current_state = client.get_state()
+    current_image = client.get_image()
+    # print(current_image)
+    
+    # action = int(input("Manual drive!: "))
+    client.interpret_actions(1)
+    car_state = client.get_car_state()
+    client.act()
+
+    print("Reward gotten: {}".format(env.compute_reward()))
+    print("is_done: {}".format(env.is_done()))
+
+    # print(car_state)
+    collision_info = client.get_collision_info()
+    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
+    print(collision_info)
+    # print(client.car_controls)
     time.sleep(1)
     # get state of the car
     # action = agent.act(current_state)
@@ -28,3 +46,44 @@ while True:
 
     # responses = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.DepthPerspective, True, False)])
     # current_state = transform_input(responses)
+
+
+# Sample of current_image
+""" 
+[[ 7  8  8 ...  0  0  0]                           
+ [ 8  8  8 ...  0  0  0]
+ [ 8  8  8 ...  0  0  0]                                                                              
+ ...             
+ [91 92 93 ... 93 92 91]
+ [94 95 96 ... 96 95 94]                                                                              
+ [96 97 98 ... 98 97 96]] 
+"""
+
+# Sample of car_state
+"""
+{   'gear': 0,                                                                            
+    'handbrake': False,                                                                               
+    'kinematics_estimated': <KinematicsState> {   'angular_acceleration': <Vector3r> {   'x_val': 0.0,                                                                                                     
+    'y_val': 0.0, 
+    'z_val': 0.0},                                                                                    
+    'angular_velocity': <Vector3r> {   'x_val': 0.0,                                                                                                                                                       
+    'y_val': 0.0,  
+    'z_val': 0.0},                                 
+    'linear_acceleration': <Vector3r> {   'x_val': 0.0,                                                                                                                                                    
+    'y_val': 0.0,                                  
+    'z_val': 0.0},                                 
+    'linear_velocity': <Vector3r> {   'x_val': 0.0,                                                                                                                                                        
+    'y_val': 0.0,                                  
+    'z_val': -0.0},                                
+    'orientation': <Quaternionr> {   'w_val': 1.0,                                                   
+    'x_val': 3.14368044200819e-05,                                                                   
+    'y_val': 5.9604641222676946e-08,                                                                 
+    'z_val': -1.873779343491977e-12},
+    'position': <Vector3r> {   'x_val': 0.0,                                                         
+    'y_val': 4.577636616431846e-07, 
+    'z_val': 0.24152274429798126}},  
+    'maxrpm': 7500.0,                              
+    'rpm': 0.0,                                                                                       
+    'speed': 0.0,                                                                                     
+    'timestamp': 1602734345355201000}
+"""
